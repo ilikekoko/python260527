5dc23a6c1fbe0afac920d5b64f3cbb315f829aa3
coco <coco@example.com>
2026-05-28 13:39:51 +0900

    웹크롤링 연습 2

--- crowl/web2.py
+++ crowl/web2.py
@@ -12,21 +12,40 @@ import urllib.request
 # re 추가
 import re
 
-# url https://www.clien.net/service/board/sold 변수처리
-url = "https://www.clien.net/service/board/sold"
-# url 열고 내용 읽어서 data 변수에 담기 헤더를 추가하여 웹 페이지를 열 때 브라우저에서 접속하는 것처럼 보이도록 설정하기
-headers = {"User-Agent": "Mozilla/5.0"}
-req = urllib.request.Request(url, headers=headers)
-with urllib.request.urlopen(req) as response:
-    data = response.read().decode("utf-8")
 
-# data를 html.parser로 분석하기
-soup = BeautifulSoup(data, "html.parser")
-# 필터링 작업 수행 span 태그 중에서 class 속성이 data-role="list-title-text" 인 태그 검색
-span_tags = soup.find_all("span", attrs={"data-role": "list-title-text"})
-# 검색된 span 태그에서 텍스트만 추출하여 출력하기
-for span in span_tags:
-    # 정규표현식으로 텍스트에서 특수문자 제거하기
-    text = re.sub(r'[^\w\s]', '', span.get_text(strip=True))
-    print(text)
+# 파일에 저장위해 파일열기 (write text) 인코딩추가하기  
+file = open("output.txt", "w", encoding="utf-8")
+
+# 반복 1 부터 10 까지 수행하기
+for i in range(1,11):
+    # url 변수에 저장하기
+# url https://www.clien.net/service/board/sold?&od=T31&category=0&po= + i 값 추가변수처리  
+    url = "https://www.todayhumor.co.kr/board/list.php?kind=total&table=total&page=" + str(i)
+    # url 열고 내용 읽어서 data 변수에 담기 헤더를 추가하여 웹 페이지를 열 때 브라우저에서 접속하는 것처럼 보이도록 설정하기
+    headers = {"User-Agent": "Mozilla/5.0"}
+    req = urllib.request.Request(url, headers=headers)
+    with urllib.request.urlopen(req) as response:
+        data = response.read().decode("utf-8")
+
+    # data를 html.parser로 분석하기
+    soup = BeautifulSoup(data, "html.parser")
+    # 필터링 작업 수행 td 태그 중에서 class="subject" 인 태그 검색하기 
+    td_tags = soup.find_all("td", class_="subject")
+    for td in td_tags:
+        # td 태그 안에 있는 a 태그 검색하기
+        atag = td.find("a")
+        # 정규표현식으로 텍스트에서 특수문자 제거하기
+        text = re.sub(r'[^\w\s]', '', atag.get_text(strip=True))
+        # re를 사용하여 문자열 검색 (정규표현식으로 단백질 글자가 포함된 텍스트만 출력하기) 
+
+        if re.search(r'단백질', text):
+            print(text)
+            # 파일에 저장하기
+            file.write(text + "\n")
+
+# 파일 닫기
+file.close()
+
+
+
 
